bbf8a06337b3791d89f56220f9309de7fe017c18
BokaiWang <bk.wang1989@gmail.com>
2021-05-01 11:07:26 -0400

    Create users component

---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class NavBar extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+  render() {
+    return (
+      <div className="navbar bg-primary">
+        <h1>
+          <i className={this.props.icon} />
+          {this.props.title}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
