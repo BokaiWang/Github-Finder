454c4e4718bec02bec7cdeb5b299608370ad4488
BokaiWang <bk.wang1989@gmail.com>
2021-05-02 10:09:05 -0400

    Create a clear button, and destructure

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import "./App.css";
 
 const github = axios.create({
   baseURL: "https://api.github.com",
-  timeout: 1000,
   headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
 });
 
@@ -21,16 +20,27 @@ class App extends Component {
   searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await github.get(`/search/users?q=${text}`);
+    // The data we need is stored in res.data.items
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <NavBar icon="fab fa-github" title="Hello" />
         <div className="container">
-          <Search searchUsers={this.searchUsers}></Search>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          ></Search>
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
